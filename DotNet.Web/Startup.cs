fa75dcfe96fb0bf23bbe2e4a6846871078e08e1c
Ernest Fakudze <ernie@ernieboy.com>
2017-01-14 17:58:46 +0000

    Add project files.

---
+++ DotNet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNet.Web.Startup))]
+namespace DotNet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
